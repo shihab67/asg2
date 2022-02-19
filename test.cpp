154de631e1271e534e690dc514ec49f968c066dd
Sayedur Rahman Shihab <s.rahman3443@gmail.com>
2022-02-19 19:25:11 +0600

    done

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <chrono>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
@@ -8,7 +8,7 @@ int main()
 {
 	int size[3] = {100000, 1000000, 10000000};
 	int i, j, k;
-	cout << sizeof(size) / sizeof(size[0]) << endl;
+	int results[3][10][3];
 
 	for (i = 0; i < 10; i++)
 	{
@@ -22,13 +22,9 @@ int main()
 				array[j] = rand() % size[k];
 			}
 
-			auto start = chrono::steady_clock::now();
+			std::clock_t start = std::clock();
 			sort(array, array + size[k]);
-			auto end = chrono::steady_clock::now();
-
-			cout << "Random Order:" << endl;
-			cout << "Size: " << size[k] << endl;
-			cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+			results[k][i][0] = (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
 
 			// ascendig order
 			for (j = 0; j < size[k]; j++)
@@ -36,12 +32,9 @@ int main()
 				array[j] = j;
 			}
 
-			start = chrono::steady_clock::now();
+			start = std::clock();
 			sort(array, array + size[k]);
-			end = chrono::steady_clock::now();
-
-			cout << "Ascending Order:" << endl;
-			cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+			results[k][i][1] = (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
 
 			// inverse order
 			for (j = 0; j < size[k]; j++)
@@ -49,15 +42,33 @@ int main()
 				array[j] = size[k] - j;
 			}
 
-			start = chrono::steady_clock::now();
+			start = std::clock();
 			sort(array, array + size[k]);
-			end = chrono::steady_clock::now();
-
-			cout << "Inverse Order:" << endl;
-			cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " milliseconds" << endl;
+			results[k][i][2] = (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
 
 			delete[] array;
 		}
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%-20s%-20s%-20s%-20s\n", "", "Random", "Ascending", "Inverse");
+		for (j = 0; j < 10; j++)
+		{
+			char s0[11];
+			char s1[11];
+			char s2[11];
+			char s3[11];
+			sprintf(s0, "%d", j + 1);
+			sprintf(s1, "%d", results[i][j][0]);
+			sprintf(s2, "%d", results[i][j][1]);
+			sprintf(s3, "%d", results[i][j][2]);
+			printf("%-20s%-20s%-20s%-20s\n", s0, s1, s2, s3);
+
+			printf("\n");
+		}
+
+		printf("\n\n");
 	}
 
 	return 0;
